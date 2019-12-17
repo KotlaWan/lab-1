34f06636ebc68b0c96e6c1ebcf530a7d0b53fb6a
KotlaWan <44785675+KotlaWan@users.noreply.github.com>
2019-12-17 19:40:41 +0300

    Add files via upload

---
+++ lab_2_1/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_2_1
+{
+    class Message
+    {
+        public string Username { get; set; }
+        public string Text { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Username}: {Text}";
+        }
+    }
+}
---
+++ lab_2_1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace lab_2_1
+{
+    public class Program
+    {
+        public static DateTime time;
+        static async Task Main()
+        {
+            Console.WriteLine("Enter username:");
+            string username = Console.ReadLine();
+            Console.Clear();
+            time = DateTime.UtcNow;
+            _ = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    if (Console.CursorLeft == 0) await ShowMessages(username);
+                    await Task.Delay(1000);
+                }
+            });
+            while (true)
+            {
+                string text = Console.ReadLine();
+                if (text != "")
+                {
+                    using FileStream fs = new FileStream(@"Messages\" + DateTime.Now.ToFileTimeUtc() + ".json", FileMode.Create);
+                    var options = new JsonSerializerOptions
+                    {
+                        
+                    };
+                     fs.Write(JsonSerializer.SerializeToUtf8Bytes<Message>(new Message
+                    {
+                        Username = username,
+                        Text = text,
+                    }));
+                }
+                int currLine = Console.CursorTop - 1;
+                Console.SetCursorPosition(0, currLine);
+                Console.Write(new string(' ', Console.WindowWidth));
+                Console.SetCursorPosition(0, currLine);
+                Console.CursorLeft = 0;
+                await ShowMessages(username);
+            }
+        }
+
+        static async Task ShowMessages(string username)
+        {
+            string[] filenames = Directory.GetFiles("Messages");
+            foreach (string filename in filenames)
+            {
+                if (time < DateTime.FromFileTimeUtc(Convert.ToInt64(filename.Split('.', '\\')[1])))
+                {
+                    FileInfo file = new FileInfo(filename);
+                    using FileStream fs = new FileStream(filename, FileMode.OpenOrCreate);
+                    Message message = await JsonSerializer.DeserializeAsync<Message>(fs);
+                    Console.WriteLine(message.Username == username ? message.Text : message.ToString());
+                }
+            }
+            time = DateTime.UtcNow;
+        }
+    }
+}
